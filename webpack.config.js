14708de02520fd89f9c6da1813a5c6d1c748e352
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-23 11:38:21 -0400

    Added public output

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+//var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    index: ['./docs/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        //exclude: /node_modules/,
+        include: /client/,
+        loader: 'babel-loader',
+        query: {
+          // https://github.com/babel/babel-loader#options
+          cacheDirectory: true,
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+
+};
